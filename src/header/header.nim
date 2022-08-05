d887b00b0d0c8f397b0b996fbbf41c0c094018e2
TynanGuenther <tgunther701@gmail.com>
2022-08-05 13:44:54 -0600

    Drawing borders around each column

--- src/header/header.nim
+++ src/header/header.nim
@@ -10,20 +10,37 @@ const
   border_tr = "┐"
   border_h = "─"
   border_v = "│"
+  start_cursor = (0,0)
 var
-  *border_length: int
-  *border_height: int
+  column_length: int
+  column_height: int
+   
+  
+  
 type
   Column = object
     title: string
     width: int
 
 
-proc draw_column(col: Column): string =
+proc draw_column(col: Column) =
+  #top
+  set_cursor_pos(1,2)
+  stdout.write(border_tl & border_h.repeat(column_length - 2) & border_tr)
   
-proc new_column(title: string): Column=
-  col:Column = Column(title:title, width:title.len+2)
-  draw_column(col)
-
-
+  #sides
+  for dy in 1..column_height-2:
+    set_cursor_pos(0, 0)
+    stdout.write(border_v)
+    set_cursor_pos(x + column_length - 1, y + dy)
+    stdout.write(border_v)
+  #bottom
+  set_cursor_pos(x, column_height - 1 + y)
+  stdout.write(border_bl & border_h.repeat(column_length - 2) & border_br)
     
+proc new_column(title: string)=
+  var colun: Column
+  colun = Column(title:title, width:title.len+2)
+  draw_column(colun)
+  
+
