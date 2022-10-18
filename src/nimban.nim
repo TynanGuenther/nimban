696cb863a4a74aa6df9798268c5697d74176c869
TynanGuenther <tgunther701@gmail.com>
2022-10-18 14:38:32 -0600

    Refining Print Column to work better

--- src/nimban.nim
+++ src/nimban.nim
@@ -14,19 +14,22 @@ const
 type
   #
   Column = object
-    title: string
-    width: int
+    title: Title
+    height: int
     id: int
     list: seq[string]
-  Cursor_Pos = tuple
-    x: int
-    y: int 
+  Title = object
+    line: string
+    num: int
+  #Cursor_Pos = tuple
+   # x: int
+   # y: int 
 
 var
-  column_width: int
-  column_height: int
+  column_width: int = 17
+  column_height: int 
   total_width: int
-  start_cursor: Cursor_Pos=(0,0)
+  #start_cursor: Cursor_Pos=(0,0)
   columns:seq[Column]
   column_count: int 
 
@@ -39,14 +42,21 @@ proc word_count(title: string): int=
       word = true
       result += 1
   
-proc print_title(title: string): int=
-  var num: int = word_count(title)
-  var done:string = ""
-  var line_num:int = 0
-  if title.len < 15:
-    stdout.write(border_v & " " & title & " " & border_v)
+proc new_title(title: string) : Title=
+  var line_num:int = 1
+  if title.len < 13:
+    result = Title(line:title, num:line_num)
   else:
     var sep_title:seq[string] = title.split(sep="(\\w{15,})+",1)
+    line_num = sep_title.len()
+    result = Title(line:title, num: line_num)
+proc print_title(title: Title)=
+  var done:string = ""
+  var line_num:int = 0
+  if title.line.len < 13:
+    stdout.write(border_v & " " & title.line & " " & border_v)
+  else:
+    var sep_title:seq[string] = title.line.split(sep="(\\w{15,})+",1)
     line_num = sep_title.len()
     for line in 0..sep_title.len()-1:
       var curr:string = sep_title[line]
@@ -54,33 +64,29 @@ proc print_title(title: string): int=
         sep_title[line].add("- " & border_v & ("\r"))
         done = done & border_v & " " & sep_title[line]
       else:
-        sep_title[line].add("\r ")
-        done = done & border_v & " " & sep_title[line] & border_v
+        sep_title[line].add(border_v & "\r ")
+        done = done & border_v & " " & sep_title[line]
+  done = done & border_bl & border_h.repeat(column_width - 2) & border_br
   stdout.write(done)
-  return line_num
   
 proc draw_column(col: Column) =
   #top
   set_cursor_pos(total_width+1, 0)
   stdout.write(border_tl & border_h.repeat(column_width - 2) & border_tr)
-  
+  print_title(col.title)
   #sides
-  for dy in 1..column_height-2:
+  for dy in col.title.num + 2..column_height-2:
     set_cursor_pos(total_width+1, dy)
-    if dy > 2 and  dy < 3:
-      #find way to print title between number of lines that title takes up ie dy > 2 and dy < number_of_lines
-      #print_title
-      stdout.write(border_v & " " & col.title & " " & border_v)
-      continue
     stdout.write(border_v)
-    set_cursor_pos(total_width + 1 + col.width, dy)
+    set_cursor_pos(total_width + 1 + column_width, dy)
     stdout.write(border_v)
   #bottom
   set_cursor_pos(total_width+1, column_height)
   stdout.write(border_bl & border_h.repeat(column_width - 2) & border_br)
   
+  total_width = total_width + column_width
   #Set cursor back to the begining after drawing
-  set_cursor_pos(start_cursor.x, start_cursor.y)
+  set_cursor_pos(0,0)
 
 #[proc new_column(title: string)=
   var col: Column
@@ -91,36 +97,31 @@ proc draw_column(col: Column) =
   draw_column(col)]#
 
 proc show()=
-  #TODO
+  for col in 0..columns.len():
+    draw_column(columns[col])
   return
 
 proc add_item(column_id: int, item: string)=
   #TODO
-  var col:Column = columns[column_id-1]
-  col.list.add(item)
-  return
-proc new_column(title: string)=
+  columns[column_id-1].list.add(item)
+proc new_column(title: Title)=
   var col: Column
   column_count+=1
   var col_list = newSeq[string](0)
-  col = Column(title:title, width:title.len+2, id:column_count, list:col_list)
+  col = Column(title: title, height: (col_list.len * 2) + title.num, id:column_count, list:col_list)
   columns.add(col)
  
 
 proc remove_item(column_id: int, item_id: int)=
   #TODO
-  var col:Column = columns[column_id-1]
-  col.list.delete(item_id)
-  return
+  columns[column_id-1].list.delete(item_id)
 
 proc remove_column(column_id: int)=
   #TODO
   columns.delete(column_id-1)
-  return
 
 proc help()=
   #TODO
-  return
   
 proc process_args(args: seq[string])=
   if paramCount() < 1:
@@ -134,7 +135,7 @@ proc process_args(args: seq[string])=
       add_item(parseInt(paramStr(i+1)),paramStr(i+2))
       continue
     of "c":
-      new_column(paramStr(i+1))
+      new_column(new_title(paramStr(i+1))
       continue
     of "r":
       var item_id:int = parseInt(paramStr(i+2))
